167c6b73c83df69f49fb6c2600d2a6448bdabf4d
ejesposito <ejesposito@debtech.com.ar>
2017-04-19 18:19:28 -0300

    Working in the structure of the project

---
+++ app/controllers/api/Administrators.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers.api;
+
+
+import com.avaje.ebean.Ebean;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.List;
+import models.Administrator;
+import play.Logger;
+import play.data.Form;
+import play.i18n.Messages;
+import play.mvc.Result;
+import static play.mvc.Results.badRequest;
+import static play.mvc.Results.created;
+import static play.mvc.Results.internalServerError;
+import static play.mvc.Results.notFound;
+import static play.mvc.Results.ok;
+
+/**
+ *
+ * @author ejesposito
+ */
+
+public class Administrators {
+    
+    private static final Logger.ALogger appLogger = Logger.of("application");
+    
+    public static Result list() {  
+        try {
+            List<Administrator> objects = Administrator.getList();
+            JsonNode jsonObjects = Administrator.jsonListSerialization(objects);
+            return ok(jsonObjects);
+        }catch(Exception e) {
+            appLogger.error("Error listing objects",e);
+            return internalServerError("Error listing objects"); 
+        }
+    }     
+    
+    public static Result get(Long id) {  
+        try {
+            Administrator object = Administrator.findByPropertie("id", id);
+            JsonNode jsonObject = object.jsonSerialization();
+            return ok(jsonObject);
+        } catch (Exception e) {
+            appLogger.error("Error getting object",e);
+            return notFound("Error getting object"); 
+        }
+    }
+    
+    public static Result create() {
+        // Get the form from the request
+        Form<Administrator> form = Form.form(Administrator.class,Administrator.creation.class).bindFromRequest();
+        // Validate errors
+        if(form.hasErrors()) {
+            return badRequest(form.errorsAsJson());
+        }
+        // Get the object from the form
+        Administrator object = form.get();
+        // Create the object in db
+        Ebean.beginTransaction();
+        try {
+            Administrator.create(object);
+            JsonNode jsonObject = object.jsonSerialization();
+            Ebean.commitTransaction();
+            return created(jsonObject);
+        } catch (Exception e) {
+            appLogger.error(Messages.get("Error creating object"), e);
+            return internalServerError("Error creating object");
+        } finally {
+            Ebean.endTransaction();
+        }
+    }
+    
+    public static Result update(Long id) {
+        // Get the form from the request
+        Form<Administrator> form = Form.form(Administrator.class,Administrator.creation.class).bindFromRequest();
+        // Validate errors
+        if(form.hasErrors()) {
+            return badRequest(form.errorsAsJson());
+        }
+        // Get the object from the form
+        Administrator object = form.get();
+        // Create the object in db
+        Ebean.beginTransaction();
+        try {
+            Administrator.update(object);
+            JsonNode jsonObject = object.jsonSerialization();
+            Ebean.commitTransaction();
+            return ok(jsonObject);
+        } catch (Exception e) {
+            appLogger.error(Messages.get("Error updating object"), e);
+            return internalServerError("Error updating object");
+        } finally {
+            Ebean.endTransaction();
+        }
+    }
+    
+    public static Result delete(Long id) {
+        Administrator object;
+        // Find the object
+        try {
+            object = Administrator.findByPropertie("id", id);
+        } catch (Exception e) {
+            return notFound("Error deleting object. Object not found");
+        }
+        // Delete the object from db
+        Ebean.beginTransaction();
+        try {
+            Administrator.delete(object);
+            Ebean.commitTransaction();
+            return ok();            
+        } catch (Exception e) {
+            appLogger.error("Error deleting object", e);
+            return internalServerError("Error deleting object");
+        } finally {
+            Ebean.endTransaction();
+        }
+    }
+    
+}
---
+++ app/models/Role.java
@@ -0,0 +1,133 @@
+package models;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import play.db.ebean.Model;
+
+@Entity
+@Table(name="roles")
+public class Role extends Model 
+{
+
+    public interface creation{}
+    
+    @Id
+    private Long id;
+    
+    private static final Finder<Long, Role> finder = new Finder<>(Long.class, Role.class);
+    
+    public static void create (Role object) throws Exception       
+    {
+        object.save();
+    }
+    
+    public static void update (Role object) throws Exception
+    {
+        object.update();
+    }
+    
+    public static void delete (Role object) throws Exception
+    {
+        object.delete();
+    }
+    
+    public static Role findByPropertie(String key,Object obj) throws Exception
+    {
+        return finder.where().eq(key,obj).findUnique();
+    }
+    
+    public static List<Role> getList () throws Exception
+    {
+        return finder.all();
+    }
+    
+   /**
+     * Serialize all the class attributes
+     * @return
+     * @throws Exception 
+     */
+    public JsonNode jsonSerialization() throws Exception
+    {
+        JsonNode jsonError;
+        ObjectMapper mapper = new ObjectMapper();
+        DateFormat myDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        mapper.setDateFormat(myDateFormat);       
+        jsonError = mapper.convertValue(this, JsonNode.class);
+        return jsonError;
+    }
+    
+   /**
+     * Serialize a list of objects with a particular set of attributes
+     * @param view
+     * @return
+     * @throws IOException 
+     */
+    public JsonNode jsonSerialization(Class view) throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        DateFormat myDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        mapper.setDateFormat(myDateFormat); 
+        mapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+        
+        ObjectWriter writer = mapper.writerWithView(view);
+        JsonNode response = mapper.readTree(writer.writeValueAsString(this));
+        return response;
+    }
+        
+   /**
+     * Serialize a list of objects with all the class attributes 
+     * @param objects
+     * @return
+     * @throws Exception 
+     */
+    public static JsonNode jsonListSerialization(List<Role> objects) throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        DateFormat myDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        mapper.setDateFormat(myDateFormat); 
+        return mapper.convertValue(objects, JsonNode.class);
+    }
+    
+   /**
+     * Des-Serialize an object who was in json format
+     * @param jsonObject
+     * @return
+     * @throws Exception 
+     */
+    public static Role jsonDesSerialization(JsonNode jsonObject) throws Exception
+    {
+        Role object;
+        ObjectMapper mapper = new ObjectMapper();
+        DateFormat myDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        mapper.setDateFormat(myDateFormat); 
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        object = mapper.convertValue(jsonObject,Role.class);
+        return object;
+    }
+    
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+}
